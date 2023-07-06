41b270ce88af9d05999cb746de5950c630e16a1a
Keno Hassler <40292329+kenohassler@users.noreply.github.com>
2023-07-06 11:44:51 +0200

    add package download feature

--- src/DataGather/__init__.py
+++ src/DataGather/__init__.py
@@ -4,17 +4,18 @@ from argparse import ArgumentParser
 from shutil import unpack_archive, rmtree
 from subprocess import run
 from pathlib import Path
-from os import mkdir
+from os import mkdir, _exit
+from urllib import request, error
 
 
-def extract_deb(filename: str, target: str) -> None:
+def extract_deb(deb_file: Path, target: Path) -> None:
     data_file = Path("data.tar.xz")
-    run(["ar", "x", filename, str(data_file)], check=True)
+    run(["ar", "x", deb_file, str(data_file)], check=True)
     unpack_archive(data_file, extract_dir=target)
     data_file.unlink()
 
 
-def unstrip_debs(binary: str, dbgsym: str,output_dir :str) -> None:
+def unstrip_debs(binary: Path, dbgsym: Path, output_dir: str) -> None:
     bin_p = Path("bin")
     dbg_p = Path("dbg")
     extract_deb(binary, bin_p)
@@ -27,8 +28,9 @@ def unstrip_debs(binary: str, dbgsym: str,output_dir :str) -> None:
     for bin in bin_p.glob("usr/bin/*"):
         if bin.is_file():
             # find debug hash
-            res = run(["eu-unstrip", "-n", "-e", str(bin)], capture_output=True, text=True)
-            if not str(bin) in res.stdout:
+            res = run(["eu-unstrip", "-n", "-e", str(bin)],
+                      capture_output=True, text=True)
+            if str(bin) not in res.stdout:
                 # bin is probably not an ELF executable
                 print(f"no build hash for {str(bin)}, skipping")
                 continue
@@ -42,7 +44,8 @@ def unstrip_debs(binary: str, dbgsym: str,output_dir :str) -> None:
             # actually unstrip
             dbg = dbg_p / "usr/lib/debug/.build-id" / prefix / dbg_file
             assert(dbg.exists())
-            res = run(["eu-unstrip", str(bin), str(dbg), "-o", str(temp_path / bin.name)], check=True)
+            run(["eu-unstrip", str(bin), str(dbg), "-o", str(temp_path / bin.name)],
+                check=True)
 
     rmtree(bin_p)
     rmtree(dbg_p)
@@ -50,8 +53,33 @@ def unstrip_debs(binary: str, dbgsym: str,output_dir :str) -> None:
     rmtree(temp_path)
 
 
+def download_pkg(name: str) -> tuple[Path, Path]:
+    # this only works if local system is target OS :/
+    # run(["apt-get", "download", name], check=True)
+    # run(["apt-get", "download", name + "-dbg"], check=True)
 
-def create_sample_structure(input_folder_path: str, output_folder_path: str):
+    # super quick and dirty apt download ^^
+    repo_url = "http://ftp.de.debian.org/debian/pool/main/"
+    name_comp = name.split("_")
+    basename = name_comp[0].split("-")[0].removeprefix("lib")
+    
+    url_dir = repo_url + basename[0] + "/" + basename + "/"
+    name_comp[0] += "-dbg"
+    dbg_name = "_".join(name_comp)
+    
+    # print(f"downloading from: {url_dir + name}")
+    # if the base package download fails, it's probably due to the string hackery above
+    request.urlretrieve(url_dir + name, filename=name)
+    try:
+        request.urlretrieve(url_dir + dbg_name, filename=dbg_name)
+    except error.HTTPError:
+        print(f"Debugging symbols not found, exiting. ({dbg_name})")
+        _exit(1)
+
+    return(Path(name), Path(dbg_name))
+
+
+def create_sample_structure(input_folder_path: str, output_folder_path: str) -> None:
 
     if not os.path.exists(output_folder_path):
         os.mkdir(output_folder_path)
@@ -78,13 +106,15 @@ def create_sample_structure(input_folder_path: str, output_folder_path: str):
 
 
 def main() -> None:
-    parser = ArgumentParser()
-    parser.add_argument('binary_package')
-    parser.add_argument('dbgsym_package')
+    parser = ArgumentParser(description=
+        "Downloads Debian packages, extracts binaries and adds debugging symbols.")
+    parser.add_argument("package", help=
+        "The full name of the debian package to be retrieved, including '.deb'.")
     parser.add_argument('output_dir',default="unstripped" ,nargs='?')
 
     args = parser.parse_args()
-    unstrip_debs(args.binary_package, args.dbgsym_package, args.output_dir)
+    binary, dbgsym = download_pkg(args.package)
+    unstrip_debs(binary, dbgsym, args.output_dir)
 
 
 if __name__ == "__main__":
